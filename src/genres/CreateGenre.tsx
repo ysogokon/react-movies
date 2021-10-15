62d721783f8a6c30cbd929cb44781b520b90713b
Yuriy Sogokon <ysogokon@honovi.com>
2021-10-15 16:01:27 -0400

    Implementing forms
    Signed-off-by: Yuriy Sogokon <ysogokon@honovi.com>

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -1,20 +1,43 @@
-import { useHistory } from 'react-router-dom';
+import { Form, Formik } from 'formik';
+import { Link } from 'react-router-dom';
 import Button from '../utils/Button';
+import * as Yup from 'yup';
+import TextField from '../forms/TextField';
 
 export default function CreateGenre() {
-  const history = useHistory();
+  // const history = useHistory();
 
   return (
     <>
       <h3>Create Genre</h3>
-      <Button
+
+      <Formik
+        initialValues={{ name: '' }}
+        onSubmit={value => {
+          // form submitted
+          console.log(value);
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required('Name field is required'),
+        })}
+      >
+        <Form>
+          <TextField field="name" displayName="Name"></TextField>
+          <Button type="submit">Save Changes</Button>
+          <Link className="btn btn-secondary" to="/genres">
+            Cancel
+          </Link>
+        </Form>
+      </Formik>
+
+      {/* <Button
         onClick={() => {
           // ...saving in DB
           history.push('/genres');
         }}
       >
         Save Changes
-      </Button>
+      </Button> */}
     </>
   );
 }
